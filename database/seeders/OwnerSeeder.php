54fc9368d6e9dcb7c6b05ab7ec4e5d4be6e22db3
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-07 20:36:23 +0900

    admin,ownerそれぞれのseederを作成してDBに登録した

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('owners')->insert([
+            [
+                'name' => 'test1',
+                'email' => 'admin_test1@example.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2024/02/07 22:00:00'
+            ],
+            [
+                'name' => 'test2',
+                'email' => 'admin_test2@example.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2024/02/07 22:00:00'
+            ],
+            [
+                'name' => 'test3',
+                'email' => 'admin_test3@example.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2024/02/07 22:00:00'
+            ]
+        ]);
+    }
+}
