5e2a369ab3992b98acd6204fb6f8f98d11365bb2
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-02-10 19:14:08 +0900

    Flash Messageのスタイル修正

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -10,7 +10,7 @@
 @endphp
 
 @if (session('message'))
-    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 rounded text-white">
+    <div class="{{ $bgColor }} w-1/2 mx-auto p-2 rounded text-white text-center">
         {{ session('message') }}
     </div>
 @endif
